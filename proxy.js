be553e54b386f8276a95df4b5ddfd273955de976
Humayun Javed <arnetraff@gmail.com>
2021-04-13 17:21:55 +0500

    Enabled https

--- proxy.js
+++ proxy.js
@@ -6,14 +6,14 @@ const httpStart = require("./http-ipc");
 var proxy = redbird({
   port: 80,
   
-  // letsencrypt: {
-  //   path: __dirname + "/certs",
-  //   port: 9999, // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
-  // },
-  // ssl: {
-  //   // http2: true,
-  //   port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
-  // },
+  letsencrypt: {
+    path: __dirname + "/certs",
+    port: 9999, // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
+  },
+  ssl: {
+    // http2: true,
+    port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
+  },
 });
 
 proxy.register("recrutability.com", "http://127.0.0.1:3000", {
