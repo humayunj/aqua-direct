5a33bd6553e9d07721d0bb6559228a7355ffc969
Humayun Javed <arnetraff@gmail.com>
2021-05-16 04:33:29 +0500

    Changed return type

--- store.js
+++ store.js
@@ -97,7 +97,7 @@ var CustomDomainsResolver = function (host, url, req) {
   // //   // http protocol? redbird will redirect
   // {
   //   console.log('Redirect:', `https://${req.headers.host}${url}`);
-    return TARGET;
+    return;
   // }
 
   let u = getUsername(host);
