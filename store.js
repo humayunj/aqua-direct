f7b076af313bfbcff42dfb9553bb30a08345a2bf
Humayun Javed <arnetraff@gmail.com>
2021-05-16 03:25:19 +0500

    Changed priority

--- store.js
+++ store.js
@@ -109,7 +109,7 @@ var CustomDomainsResolver = function (host, url, req) {
     };
   }
 };
-CustomDomainsResolver.priority = 1;
+CustomDomainsResolver.priority = 0;
 
 function store(proxy) {
   proxy.addResolver(CustomDomainsResolver);
